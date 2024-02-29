e6c77fba722f1ed431ff4cbd2065d37597c3c7b2
Slava <avs-spider@ya.ru>
2024-02-29 20:22:32 +0300

    Edit Models in models.py

--- pereval/pereval_app/admin.py
+++ pereval/pereval_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Turist)
+admin.site.register(Coords)
+admin.site.register(Images)
+admin.site.register(Level)
+admin.site.register(Mounts)
 
 # Register your models here.
