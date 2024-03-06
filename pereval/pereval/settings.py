e7b52a25228d64ee91ce2cebf8bbbcb9bcece06d
Slava <avs-spider@ya.ru>
2024-03-06 20:11:43 +0300

    Добавление Databases

--- pereval/pereval/settings.py
+++ pereval/pereval/settings.py
@@ -82,17 +82,22 @@ WSGI_APPLICATION = 'pereval.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'perevals',  #Имя БД
-        'USER': 'postgres', #Имя пользователя в БД
-        'PASSWORD': '07Drose1', #пароль БД
-        'HOST': 'localhost',
-        'PORT': '5433'
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'perevals',  #Имя БД
+#         'USER': 'postgres', #Имя пользователя в БД
+#         'PASSWORD': '07Drose1', #пароль БД
+#         'HOST': 'localhost',
+#         'PORT': '5433'
+#     }
+# }
 
 
 # Password validation
